7802e4bd3f557bc0403d052709eef1d9c7901a2a
WellingtonFR <wellingtondfr@gmail.com>
2025-02-17 16:37:16 -0300

    Alteração de config de static files do settings

--- chatgpt_project/chatgpt_project/settings.py
+++ chatgpt_project/chatgpt_project/settings.py
@@ -127,8 +127,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 SECRET_KEY = os.getenv('SECRET_KEY', '!**django-insecure**!')
