e7f12b8dffb7f6976e7ab29df79a20afaf7cba31
WellingtonFR <wellingtondfr@gmail.com>
2025-02-17 21:27:55 -0300

    Remove o allauth

--- chatgpt_project/chatgpt_project/urls.py
+++ chatgpt_project/chatgpt_project/urls.py
@@ -1,10 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from .views import google_login_view
-
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("chat.urls")),
-    path('accounts/', include('allauth.urls')),
 ]
